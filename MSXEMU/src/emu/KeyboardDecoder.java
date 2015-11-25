426597228a09272de09cea1ba0789a6ec94c6edf
tjitze <tjitze@gmail.com>
2015-11-25 17:27:01 +0100

    Various cleanup. Added comments. Completed translation matrix.

--- MSXEMU/src/emu/KeyboardDecoder.java
+++ MSXEMU/src/emu/KeyboardDecoder.java
@@ -6,8 +6,19 @@ import java.awt.event.KeyListener;
 
 public class KeyboardDecoder implements KeyListener {
 
+	/*
+	 * This array contains the MSX key matrix (11 rows of 8 bits each)
+	 * The array is updated by the pressKey and depressKey methods and
+	 * is read out by the getRowValue method.
+	 */
 	private byte[] rows = new byte[11];
 	
+	/* 
+	 * Matrix for translating characters to key matrix codes. 
+	 * Every entry is of the form {character, row, column},
+	 * meaning that character is mapped to the matrix at position
+	 * row/column.
+	 */
 	private int[][] charMatrix = {
 			{'0', 0, 0}, {')', 0, 0}, {'1', 0, 1}, {'!', 0, 1}, {'2', 0, 2}, {'@', 0, 2},
 			{'3', 0, 3}, {'#', 0, 3}, {'4', 0, 4}, {'$', 0, 4}, {'5', 0, 5}, {'%', 0, 5},
@@ -16,8 +27,11 @@ public class KeyboardDecoder implements KeyListener {
 			{'8', 1, 0}, {'*', 1, 0}, {'9', 1, 1}, {'(', 1, 1}, {'-', 1, 2}, {'_', 1, 2}, 
 			{'=', 1, 3}, {'+', 1, 3}, {'\\', 1, 4}, {'|', 1, 4}, {'[', 1, 5}, {'{', 1, 5}, 
 			
-			{'A', 2, 6}, {'B', 2, 7}, {'a', 2, 6}, {'b', 2, 7}, 
-
+			{'A', 2, 6}, {'B', 2, 7}, {'a', 2, 6}, {'b', 2, 7},
+			{'/', 2, 4}, {'?', 2, 4}, {'.', 2, 3}, {'>', 2, 3},
+			{',', 2, 2}, {'<', 2, 2}, {'`', 2, 1}, {'~', 2, 1},
+			{'\'', 2, 0}, {'"', 2, 0},
+			
 			{'C', 3, 0}, {'D', 3, 1}, {'E', 3, 2}, {'F', 3, 3}, {'G', 3, 4}, {'H', 3, 5}, 
 			{'I', 3, 6}, {'J', 3, 7}, 
 			{'c', 3, 0}, {'d', 3, 1}, {'e', 3, 2}, {'f', 3, 3}, {'g', 3, 4}, {'h', 3, 5}, 
@@ -35,6 +49,12 @@ public class KeyboardDecoder implements KeyListener {
 
 	};
 
+	/* 
+	 * Matrix for translating special keys to key matrix codes. 
+	 * Every entry is of the form {KeyEvent, row, column},
+	 * meaning that KeyEvent is mapped to the matrix at position
+	 * row/column.
+	 */
 	private int[][] keyMatrix = {
 			{KeyEvent.VK_SHIFT, 		6, 0},
 			{KeyEvent.VK_CONTROL,		6, 1},
@@ -67,17 +87,27 @@ public class KeyboardDecoder implements KeyListener {
 	};
 	
 	@Override
-	public void keyTyped(KeyEvent e) {
-	}
+	public void keyTyped(KeyEvent e) {	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		processKeyEvent(e, true);
 	}
-
 	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		processKeyEvent(e, false);
+	}
+
+	/**
+	 * Process key event.
+     *
+	 * @param e The key event.
+	 * @param state true if key pressed, false if key depressed
+	 */
 	private void processKeyEvent(KeyEvent e, boolean state) {
-		/* Try to match character */
+		
+		/* Try to match character and translate it to a pressKey/depressKey call */
 		int c = (int)e.getKeyChar();
 		boolean success = false;
 		keySearchLoop:
@@ -85,46 +115,52 @@ public class KeyboardDecoder implements KeyListener {
 			if (c == charMatrix[i][0]) {
 				if (state) {
 					pressKey(charMatrix[i][1], charMatrix[i][2]);
-					System.out.println("Press key   row: " + charMatrix[i][1] + ", bit: " + charMatrix[i][2]);
 				} else {
 					depressKey(charMatrix[i][1], charMatrix[i][2]);
-					System.out.println("Depress key row: " + charMatrix[i][1] + ", bit: " + charMatrix[i][2]);
 				}
 				success = true;
 				break keySearchLoop;
 			}
 		}	
 		
-		/* Try to match key */
+		/* Try to match key and translate it to a pressKey/depressKey call */
 		if (!success) {
 			int k = e.getKeyCode();
 			for (int i = 0; i < keyMatrix.length; i++) {
 				if (k == keyMatrix[i][0]) {
 					if (state) {
 						pressKey(keyMatrix[i][1], keyMatrix[i][2]);
-						System.out.println("Press key   row: " + keyMatrix[i][1] + ", bit: " + keyMatrix[i][2]);
 					} else {
 						depressKey(keyMatrix[i][1], keyMatrix[i][2]);
-						System.out.println("Depress key row: " + keyMatrix[i][1] + ", bit: " + keyMatrix[i][2]);
 					}
 				}
 			}
 		}
 	}
 
+	/**
+	 * Set value at given row/bit position to true.
+	 */
 	private void pressKey(int row, int bit) {
-		rows[row] = (byte)(rows[row] | (1 << bit));
+		rows[row] = (byte)((rows[row] & 0xff) | (1 << bit));
 	}
 
+	/**
+	 * Set value at given row/bit position to false.
+	 */
 	private void depressKey(int row, int bit) {
-		rows[row] = (byte)(rows[row] & ~(1 << bit));
+		rows[row] = (byte)((rows[row] & 0xff) & ~(1 << bit));
 	}
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		processKeyEvent(e, false);
-	}
-	
+	/**
+	 * Get the byte value of the given row. If a bit
+	 * is set to true, it means that the key is pressed.
+	 * (Warning: this value must be inverted according
+	 * to MSX spec)
+	 * 
+	 * @param row Value of row to get.
+	 * @return Byte containing all bits of given row.
+	 */
 	public byte getRowValue(int row) {
 		return rows[row];
 	}
