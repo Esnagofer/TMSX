50c8a8414f2d3da2549c2120cd84e584aa7dec58
tjitze <tjitze@gmail.com>
2015-12-02 16:51:50 +0100

    We can now load a system ROM from the UI. This value is saved as a preference.

--- MSXEMU/src/emu/ui/EmuFrame.java
+++ MSXEMU/src/emu/ui/EmuFrame.java
@@ -2,6 +2,7 @@ package emu.ui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -12,25 +13,37 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import emu.MSX;
+import emu.memory.EmptySlot;
+import emu.memory.RAMSlot;
 import emu.memory.ROMSlot;
 
 public class EmuFrame extends JFrame {
 
 	private MSX msx;
+	private JButton cartridgeButton;
+	private JButton sysRomButton;
+	private JButton pauseButton;
+	private JButton resetButton;
+	private JButton breakButton;
 	
 	public static void main(String[] args) {
+
+		/* Initialize MSX instance */
 		MSX msx = new MSX();
 		msx.initHardware();
 		
+		/* Build frame */
 		EmuFrame frame = new EmuFrame(msx);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(512,435);
@@ -42,7 +55,56 @@ public class EmuFrame extends JFrame {
 	        }
 	      });
 		
-	    msx.startMSX();
+	    /* Load ROM */
+	    Preferences prefs = Preferences.userRoot().node("MSXEMU");
+	    String romFile = prefs.get("msx_system_rom", "-");
+	    boolean romLoadOK = false;
+	    
+	    msx.getSlots()[0] = new ROMSlot(0xC000);
+		try {
+			msx.getSlots()[0].load(romFile, (short)0x0000, 0x8000);
+			romLoadOK = true;
+		} catch (IOException e) {
+			System.out.println("Could not load system rom \"" + romFile + "\": " + e.getMessage());
+		}
+
+		/* We fill slot 1 and 2 with empty ROM */
+		msx.getSlots()[1] = new EmptySlot();
+		msx.getSlots()[2] = new EmptySlot();
+
+		/* Slot 3 is RAM */
+		msx.getSlots()[3] = new RAMSlot();
+
+		/* If ROM was not loaded, activate ROM load state */
+		if (!romLoadOK) {
+			frame.activateROMLoadState();
+		}
+
+		/* Start MSX */
+		msx.startMSX();
+
+	}
+		
+	/**
+	 * Disable all buttons except Load System ROM
+	 */
+	public void activateROMLoadState() {
+		cartridgeButton.setEnabled(false);
+		sysRomButton.setEnabled(true);
+		pauseButton.setEnabled(false);
+		resetButton.setEnabled(false);
+		breakButton.setEnabled(false);
+	}
+
+	/**
+	 * Enable all buttons. Use after ROM is loaded
+	 */
+	public void romLoaded() {
+		cartridgeButton.setEnabled(true);
+		sysRomButton.setEnabled(true);
+		pauseButton.setEnabled(true);
+		resetButton.setEnabled(true);
+		breakButton.setEnabled(true);
 	}
 	
 	public EmuFrame(MSX msx) {
@@ -67,27 +129,30 @@ public class EmuFrame extends JFrame {
 		msxPanel.addKeyListener(msx.getKeyBoard());
 		
 		// Button panel
-		GridLayout buttonLayout = new GridLayout(1,3);
+		FlowLayout buttonLayout = new FlowLayout();
 		JPanel buttonPanel = new JPanel(buttonLayout);
 		add(buttonPanel, BorderLayout.PAGE_END);
-		JButton slot1 = new JButton("Slot 1");
-		JButton slot2 = new JButton("Slot 2");
-		JButton reset = new JButton("Reset");
-		JButton pause = new JButton("Debug");
-		JButton bbreak = new JButton("Stop");
-		buttonPanel.add(slot1);
-		buttonPanel.add(slot2);
-		buttonPanel.add(pause);
-		buttonPanel.add(reset);
-		buttonPanel.add(bbreak);
-		slot1.setFocusable(false);
-		slot2.setFocusable(false);
-		pause.setFocusable(false);
-		reset.setFocusable(false);
-		bbreak.setFocusable(false);
-		
+		cartridgeButton = new JButton("Set Cartridge");
+		sysRomButton = new JButton("Set System ROM");
+		resetButton = new JButton("Reset");
+		pauseButton = new JButton("Debug");
+		breakButton = new JButton("Stop");
+		resetButton.setPreferredSize(new Dimension(70, 25));
+		pauseButton.setPreferredSize(new Dimension(70, 25));
+		breakButton.setPreferredSize(new Dimension(70, 25));
+		buttonPanel.add(cartridgeButton);
+		buttonPanel.add(sysRomButton);
+		buttonPanel.add(pauseButton);
+		buttonPanel.add(resetButton);
+		buttonPanel.add(breakButton);
+		cartridgeButton.setFocusable(false);
+		sysRomButton.setFocusable(false);
+		pauseButton.setFocusable(false);
+		resetButton.setFocusable(false);
+		breakButton.setFocusable(false);
+
 		// Reset button listener
-		reset.addActionListener(new ActionListener() {
+		resetButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				msx.reset();
@@ -95,7 +160,7 @@ public class EmuFrame extends JFrame {
 		});
 		
 		// Pause button listener
-		pause.addActionListener(new ActionListener() {
+		pauseButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				msx.debugMode = true;
@@ -103,7 +168,7 @@ public class EmuFrame extends JFrame {
 		});
 
 		// Stop button listener (as mouse listener, so we can distinguish pressed/released events)
-		bbreak.addMouseListener(new MouseAdapter() {
+		breakButton.addMouseListener(new MouseAdapter() {
 	        @Override
 	        public void mousePressed(MouseEvent e) {
 	            msx.getKeyBoard().stopKeyPressed();
@@ -115,7 +180,7 @@ public class EmuFrame extends JFrame {
 		});	
 		
 		// Slot load listeners
-		slot1.addActionListener(new ActionListener() {
+		cartridgeButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -128,9 +193,38 @@ public class EmuFrame extends JFrame {
 		    			s.load(file.getAbsolutePath(), (short)0x4000, (int)file.length());
 		    		} catch (IOException ex) {
 		    			ex.printStackTrace();
-		    			System.exit(0);
 		    		}
 		    		msx.setSlot(1, s);
+					msx.reset();
+		        }
+			}
+			
+		});
+		
+		// System rom button listener
+		sysRomButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			    Preferences prefs = Preferences.userRoot().node("MSXEMU");
+				JFileChooser fc = new JFileChooser();
+				fc.setSelectedFile(new File(prefs.get("msx_system_rom", "~/")));
+				int returnVal = fc.showOpenDialog(EmuFrame.this);
+		        if (returnVal == JFileChooser.APPROVE_OPTION) {
+		            File file = fc.getSelectedFile();
+				    msx.getSlots()[0] = new ROMSlot(0xC000);
+					boolean romLoadOK = false;
+				    try {
+						msx.getSlots()[0].load(file.getAbsolutePath(), (short)0x0000, 0x8000);
+						romLoadOK = true;
+					} catch (IOException ex) {
+						System.out.println("Could not load system rom \"" + file.getAbsolutePath() + "\": " + ex.getMessage());
+					}
+				    if (romLoadOK) {
+					    prefs.put("msx_system_rom", file.getAbsolutePath());
+					    romLoaded();
+						msx.reset();
+				    }
 		        }
 			}
 			
